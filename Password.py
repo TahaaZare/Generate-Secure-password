23c71f15fc9db7f01a48f8fa75b914c74f8492de
Taha Zare <81828931+TahaZre@users.noreply.github.com>
2022-04-24 15:58:10 +0430

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,29 +1,21 @@
+import random
+import string
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+symbols = "@$%#&*^!@`~_++()|{}/?"
+numbers = "123456789"
+
+passwordContain = lower + upper + symbols + numbers
+
 while True:
-    print("Choose Your Option : \n\t1)Encrypt\n\t2)Decrypt\n\t3)Exit")
+    print("Choose an Option : \n\t1)Create Password\n\t2)Exit")
     choice = input("Your Choice : ")
     if choice == "1":
-        plainText = input("text : ")
-        encryptedText = ""
-        for character in plainText:
-            x = ord(character) * 8 + 21
-            encryptedText += chr(x)
-        print(f"*"*21)
-        print(f"encrypted text : {encryptedText}")
-        print(f"*"*21)
-
+        length = int(input("Enter length of password : "))
+        passwordHasher = "".join(random.sample(passwordContain,length))
+        print(passwordHasher)
     elif choice == "2":
-        encryptedText = input("Encrypted Text : ")
-        plainText = ""
-        for character in encryptedText:
-            x = (ord(character)-21)//8
-            plainText += chr(x)
-        print(f"*" * 21)
-        print(f"decrypted text : {plainText}")
-        print(f"*" * 21)
-
-    elif choice == "3":
         break
     else:
-        print(f"*"*21)
-        print("Sorry Just Choose Number 1 to 3 !!!")
-        print(f"*" * 21)
+       print("Sorry just Choose 1 or 2 !!!")
